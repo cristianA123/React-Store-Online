684f659179cbd800b844d1d91994b1478b97bf6d
cristianA123 <cristianchipan2@gmail.com>
2024-01-04 13:10:06 -0500

    Listar productos

--- src/Pages/home/index.jsx
+++ src/Pages/home/index.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react"
 import Card from "../../components/Card/INDEX.JSX"
 import Layout from "../../components/Layout"
+import { useEffect } from "react"
 
 function Home() {
+
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then((response) => response.json())
+    .then((data) => setItems(data))
+    .catch((error) => console.log(error))
+
+  }, [])
+  
 
     return (
         <Layout>
           Home
-          <Card />
+          <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+            {
+              items && items.map((item) => (
+                <Card key={item.id} data={item} />
+              ))
+            }
+          </div>
         </Layout>
       )
   }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,25 @@
 
 
-const Card = () => {
+import PropTypes from 'prop-types';
+
+const Card = ({ data }) => {
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
-        <figure className="relative mb-2 w-full h-4/5">
-            <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">Electronics</span>
-            <img className="w-full h-full object-cover rounded-lg" src="https://upload.wikimedia.org/wikipedia/commons/0/03/Eiche_bei_Graditz.jpg" alt="arbol" />
-            <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-2">+</div>
-        </figure>
-        <p className="flex justify-between">
-            <span className="text-sm font-light">Headphone</span>
-            <span className="text-sm font-medium">$300</span>
-        </p>
+      <figure className="relative mb-2 w-full h-4/5">
+        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.category.name }</span>
+        <img className="w-full h-full object-cover rounded-lg" src={data.images[0]} alt="arbol" />
+        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-2">+</div>
+      </figure>
+      <p className="flex justify-between">
+        <span className="text-sm font-light">{data.title}</span>
+        <span className="text-sm font-medium">${data.price}</span>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+Card.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default Card;
